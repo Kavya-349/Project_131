9ca4728ee03114db5190371159d5b7c36a9ad90f
Kavya-349 <83696676+Kavya-349@users.noreply.github.com>
2022-01-05 14:43:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function ac_page() {
+    window.location = "ac.html";
+}
+
+function chair_page() {
+    window.location = "chair.html";
+}
+
+function fridge_page() {
+    window.location = "fridge.html";
+}
+
+function desk_page() {
+    window.location = "desk.html";
+}
+
+function telephone_page() {
+    window.location = "telephone.html";
+}
+
+function tv_page() {
+    window.location = "tv.html";
+}
